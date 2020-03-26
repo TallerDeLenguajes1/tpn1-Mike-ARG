e81d290237cfab5933e92969e950ddc2edaf6982
Miguel <milito_vanetta@hotmail.com>
2020-03-26 14:40:52 -0300

    Corrección del punto 2 del Trabajo Práctico 1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main() {
+int main() {
     int var = 25;
     int *punt;
     punt = &var;
@@ -10,5 +10,7 @@ void main() {
     printf("Dirección de memoria de la variable: %p\n", &var);
     printf("Dirección de memoria del puntero: %p\n", &punt);
     printf("Tamaño de la memoria utilizado por la variable: %d\n", sizeof(var));
+
+    return 0;
 
 }
